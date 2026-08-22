cb40ff49d27b8ac0ebf545cfee35d5a39a59ccff
BarrieMosque <barriemosque@gmail.com>
2026-08-22 19:01:43 -0400

    Refactor nodemailer transport configuration
    Refactor nodemailer transport configuration for better readability and secure flag handling.

--- Backend/services/email.service.js
+++ Backend/services/email.service.js
@@ -3,7 +3,15 @@ import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const transporter = nodemailer.createTransport({ host: process.env.SMTP_HOST, port: process.env.SMTP_PORT, secure: true, auth: { user: process.env.SMTP_EMAIL, pass: process.env.SMTP_PASSWORD } });
+   const transporter = nodemailer.createTransport({
+       host: process.env.SMTP_HOST,
+       port: process.env.SMTP_PORT,
+       secure: process.env.SMTP_PORT == 465,
+       auth: {
+           user: process.env.SMTP_EMAIL,
+           pass: process.env.SMTP_PASSWORD
+       }
+   });
 
 export const sendMailServ = async (mailOptions) => {
     if (!mailOptions?.to || !mailOptions?.html) {
